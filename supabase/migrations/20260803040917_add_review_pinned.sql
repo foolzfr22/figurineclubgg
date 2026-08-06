0eeec219b346ad97b1c874c3ce9fe256e57b42e8
foolzfr22 <foolzfr002@gmail.com>
2026-08-07 00:23:22 +0530

    Delete supabase/migrations directory

--- supabase/migrations/20260803040917_add_review_pinned.sql
+++
@@ -1,9 +0,0 @@
-/*
-# Add is_pinned column to reviews
-
-## Changes
-- Add `is_pinned` boolean column to reviews table (default false)
-- Allows admins to pin specific reviews to the top of product pages
-*/
-
-ALTER TABLE reviews ADD COLUMN IF NOT EXISTS is_pinned boolean DEFAULT false;
